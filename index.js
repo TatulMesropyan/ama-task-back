02a0bc0bad36865566b500e540ea1b37b5bef539
t.mesropyan <tatul.mesropyan-extern@invia.de>
2024-01-12 01:50:46 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const multer = require('multer');
+const csv = require('csv-parser');
+const xml2js = require('xml2js');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 8080;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.use(express.json());
+
+app.post('/upload/:type', upload.single('file'), handleFileUpload);
+
+function handleFileUpload(req, res) {
+    try {
+        const fileBuffer = req.file.buffer;
+        // const fileType = req.params.type;
+        console.log(fileBuffer)
+        let statements;
+
+        // if (fileType === 'text/csv') {
+        //     statements = readCSV(fileBuffer);
+        // } else if (fileType === 'text/xml') {
+        //     statements = readXML(fileBuffer);
+        // } else {
+        //     return res.status(400).json({ error: 'Invalid file type. Supported types: csv, xml' });
+        // }
+
+        validateAndRespond(statements, res);
+    } catch (error) {
+        console.error('Error processing file:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+function readCSV(fileBuffer) {
+    return new Promise((resolve, reject) => {
+        const statements = [];
+
+        csv({ headers: true })
+            .on('data', (row) => statements.push(row))
+            .on('end', () => resolve(statements))
+            .on('error', (error) => reject(error))
+            .write(fileBuffer);
+    });
+}
+
+function readXML(fileBuffer) {
+    return new Promise((resolve, reject) => {
+        const xmlString = fileBuffer.toString('utf-8');
+
+        xml2js.parseString(xmlString, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                const statements = result.records.record;
+                resolve(statements);
+            }
+        });
+    });
+}
+
+app.listen(port, () => {
+    console.log(`Server listening to port: ${port}`);
+});
